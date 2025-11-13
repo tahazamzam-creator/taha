e7e2f57b68c8bd100e0a95c96cb2320f31fa0085
tahazamzam-creator <tahazmaany@gmail.com>
2025-11-13 14:42:10 +0330

    Update connect.php

--- connect.php
+++ connect.php
@@ -1,16 +1,22 @@
+
 <?php
-function connect($servername,$username,$password,$database)
+include 'connect.php';
+$x=$_POST["f_name"];
+$y=$_POST["l_name"];
+$s=$_POST["n_code"];
+$h=$_POST["father_name"];
+$f=$_POST["phone_n"];
+
+$servername="localhost";
+$username="root";
+$password="";
+$database="ahmad";
+$conn= connect("localhost","root","","");
+function ql($x , $y , $s , $h , $f, $conn)
 {
-$conn=mysqli_connect($servername,$username, $password, $database);
-  if ($conn)
-  {
-    echo"درست";
-    return ($conn);
-  }
-  else
-  {
-    echo"نادرست";
-  }
-     mysqli_connect($servername , $username , $password , $database);
+ $q="INSERT INTO taregh (f_name, l_name, n_code, father_name, phone_n)
+ VALUES ('$x' , '$y' , '$s' , '$h' , '$f')";
+ mysqli_prepare($conn, $q);
 }
+ ql($x, $y, $s , $h, $f,$conn);
 ?>
